ae63348625bdc41adddb0981457cbbf2381afaff
Tee++ <admin@jquerytips.com>
2012-03-10 23:48:22 +0700

    Tiny changed

--- examples/index.php
+++ examples/index.php
@@ -1,3 +1,19 @@
+<?php
+/**
+ * jPayment Gateway
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * It is also available through the world-wide-web at this URL:
+ * http://www.jquerytips.com/
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to admin@jquerytips.com so we can send you a copy immediately.
+ *
+ * @copyright  Copyright (c) 2011 - 2012 jQueryTips (http://www.jquerytips.com)
+ * @version    1.0.1
+ */
+ 
+?>
 <!DOCTYPE html>
 <html>
 	<head>
